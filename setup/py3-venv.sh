cd80d4dbe4a6acce3c54f6f47f0f119754b89241
brschroeder <brett.schroeder@gmail.com>
2019-10-12 13:03:07 -0700

    changed to rhel new module scheme

--- setup/py3-venv.sh
+++ setup/py3-venv.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+# Install Python 3.6 module (no user-space python is installed by default on CentOS 8)
 # Create python3 virtual environment and install email_validator
-# See https://developers.redhat.com/blog/2018/08/13/install-python3-rhel/ for
-# official Redhat recommendations on creating python3 environment
-# Examples on CentOS using software-collections-repositiory
-# found here https://linuxize.com/post/how-to-install-python-3-on-centos-7/
-# Python 3 is installed in /opt/rh/rh-python3X/root/bin
+# See Redhat docs https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html-single/installing_managing_and_removing_user_space_components/index 
+# for background info on new BaseOS vs AppStream concepts.
+# See Redhat dev blog https://developers.redhat.com/blog/2018/11/14/python-in-rhel-8/
+# for guidelines on using Python in RHEL.
+# Python 3 is installed in /usr/bin/, note that this is different from "system-python".
 
 echo Creating Python 3 virtual environment...
+hide_output yum --assumeyes --quiet install @python36
 inst_dir=/usr/local/lib/mailinabox
 mkdir -p $inst_dir
 venv=$inst_dir/env
-hide_output yum --assumeyes --quiet install centos-release-scl || exit 1
-hide_output yum --assumeyes --quiet install rh-python36
-/opt/rh/rh-python36/root/bin/python3 -m venv $venv
-# Upgrade pip because the CentOS-packaged version is out of date.
+/usr/bin/python3 -m venv /usr/local/lib/mailinabox/env
+# Upgrade pip because the CentOS-packaged version is out of date. Install wheel.
 hide_output $venv/bin/pip install --upgrade pip || exit 1
+hide_output $venv/bin/pip install wheel || exit 1
